1af96cb872ba79968f8a9aca23c369baeb359764
Alexandra Leonova <kabrenok@gmail.com>
2017-03-14 23:57:06 +0300

    Размеры индекса и каталога

--- js/main-nav.js
+++ js/main-nav.js
@@ -6,3 +6,13 @@ btn.addEventListener("click", function(event) {
   menu.classList.toggle("main-nav__list-wrap--opened");
   btn.classList.toggle("main-nav__btn--close");
 });
+
+var basket = document.querySelector(".modal-show");
+var modal = document.querySelector(".modal-window");
+var overlay = document.querySelector(".overlay");
+
+basket.addEventListener("click", function(event) {
+  event.preventDefault();
+  overlay.classList.toggle("overlay--shown");
+  modal.classList.toggle("modal-window--opened");
+});
