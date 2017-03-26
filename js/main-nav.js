67c39ca3624908a91d02c9593a8c6afa3293d83b
Alexandra Leonova <kabrenok@gmail.com>
2017-03-26 03:44:19 +0300

    Модальное окно, esc, click-закрытие

--- js/main-nav.js
+++ js/main-nav.js
@@ -13,17 +13,3 @@ var noJSLot = document.getElementsByClassName("main-nav--no-js");
   btn.classList.toggle("main-nav__btn--open");
   btn.classList.toggle("main-nav__btn--close");
 });
-
-
-var basket = document.querySelector(".modal-show");
-var modal = document.querySelector(".modal-window");
-var overlay = document.querySelector(".overlay");
-var els = document.getElementsByClassName("modal-show");
-
-for (i=0 ; i<els.length ; i++ ) {
-  els[i].addEventListener("click", function(event) {
-  event.preventDefault();
-  overlay.classList.toggle("overlay--shown");
-  modal.classList.toggle("modal-window--opened");
-});
-}
