afb2a0aedf1f686f14c406caa89ee37f3aeb6177
Alexandra Leonova <kabrenok@gmail.com>
2017-04-01 02:31:17 +0300

    Правки, карта

--- js/main-nav.js
+++ js/main-nav.js
@@ -1,15 +1,15 @@
-var btn = document.querySelector(".main-nav__btn");
-var menu = document.querySelector(".main-nav__items-links");
-var user = document.querySelector(".main-nav__user-links");
-var noJS = document.querySelector(".main-nav--no-js");
-var noJSLot = document.getElementsByClassName("main-nav--no-js");
+(function(){
+  var btn = document.querySelector(".main-nav__btn");
+  var menu = document.querySelector(".main-nav__items-links");
+  var user = document.querySelector(".main-nav__user-links");
 
   menu.classList.remove("main-nav--no-js");
   user.classList.remove("main-nav--no-js");
   btn.addEventListener("click", function(event) {
-  event.preventDefault();
-  menu.classList.toggle("main-nav__items-list--opened");
-  user.classList.toggle("main-nav__items-list--opened");
-  btn.classList.toggle("main-nav__btn--open");
-  btn.classList.toggle("main-nav__btn--close");
-});
+    event.preventDefault();
+    menu.classList.toggle("main-nav__items-list--opened");
+    user.classList.toggle("main-nav__items-list--opened");
+    btn.classList.toggle("main-nav__btn--open");
+    btn.classList.toggle("main-nav__btn--close");
+  });
+})();
