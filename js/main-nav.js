c923f0c49ed08f4440a84bac331a105f33129470
Alexandra Leonova <kabrenok@gmail.com>
2017-04-01 22:23:43 +0300

    Правки перед сборкой

--- js/main-nav.js
+++ js/main-nav.js
@@ -2,9 +2,12 @@
   var btn = document.querySelector(".main-nav__btn");
   var menu = document.querySelector(".main-nav__items-links");
   var user = document.querySelector(".main-nav__user-links");
+  var logo = document.querySelector(".logo");
 
-  menu.classList.remove("main-nav--no-js");
-  user.classList.remove("main-nav--no-js");
+  menu.classList.remove("main-nav__items-list--no-js");
+  user.classList.remove("main-nav__items-list--no-js");
+  logo.classList.remove("logo--no-js");
+  btn.classList.remove("main-nav__btn--no-js");
   btn.addEventListener("click", function(event) {
     event.preventDefault();
     menu.classList.toggle("main-nav__items-list--opened");
